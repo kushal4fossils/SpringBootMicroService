61d5a871e1c389c7d77859375f27167e3b033951
kushal4fossils <kushal4fossils@gmail.com>
2022-10-18 02:46:36 +0530

    Initial Commit

---
+++ SpringBootMicroService/src/test/java/com/idiot/SpringBootMicroService/SpringBootMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idiot.SpringBootMicroService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
