fe033d8e9d81d50919444fae5847e6d8d01a2098
kushal4fossils <kushal4fossils@gmail.com>
2022-10-27 16:18:15 +0530

    Added Request Mapping

--- SpringBootMicroService/src/main/java/com/idiot/SpringBootMicroService/controller/AdminController.java
+++ SpringBootMicroService/src/main/java/com/idiot/SpringBootMicroService/controller/AdminController.java
@@ -1,14 +1,24 @@
 package com.idiot.SpringBootMicroService.controller;
 
+import com.idiot.SpringBootMicroService.model.User;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AdminController {
     @RequestMapping("/home")
-    public String testReturn(){
-        System.out.println("Hi");
-        return "Hello World";
+    public String test() {
+        return "Helo World";
+    }
+
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public User user() {
+        User user = new User();
+        user.setId("1");
+        user.setName("Kushal");
+        user.setEmail("kushal.kb0606@gmail.com");
+        return user;
     }
 
 }
